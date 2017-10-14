7766ee03bfc36f942f73eb18a2d8c5834effc9a2
Kishin Yagami <k.yagami.suou@gmail.com>
2017-10-14 12:43:50 +0900

    Update demo site

---
+++ settings.js
@@ -0,0 +1,4 @@
+
+__LAMBSTATUS_API_URL__ = 'https://kagxah0tee.execute-api.ap-northeast-1.amazonaws.com/prod/';
+__LAMBSTATUS_USER_POOL_ID__ = 'ap-northeast-1_dkkMAmikT';
+__LAMBSTATUS_CLIENT_ID__ = '5j1ilokcr0k0qg0feadov79q0h';
